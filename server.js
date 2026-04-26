514bc03420d7211807ab324b43ed18ba147a7714
timurfaizievvv <timurfaizievvv@gmail.com>
2026-04-26 21:19:48 +0500

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,43 @@ app.use(express.static(__dirname));
 
 const PORT = process.env.PORT || 3000;
 
+function scheduleReminder(date, time, chat_id) {
+  const trainingDate = new Date(`${date} ${time}`);
+
+  const now = new Date();
+
+  const ms24h = trainingDate - now - (24 * 60 * 60 * 1000);
+  const ms2h = trainingDate - now - (2 * 60 * 60 * 1000);
+
+  // за 24 часа
+  if (ms24h > 0) {
+    setTimeout(() => {
+      fetch(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          chat_id,
+          text: `Доброго времени суток!\nНапоминаю, что завтра Вы записаны ко мне на тренировку в ${time} ✨`
+        }),
+      });
+    }, ms24h);
+  }
+
+  // за 2 часа
+  if (ms2h > 0) {
+    setTimeout(() => {
+      fetch(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          chat_id,
+          text: `Доброго времени суток!\nНапоминаю, что через 2 часа состоится наша тренировка ✨`
+        }),
+      });
+    }, ms2h);
+  }
+}
+
 // запись в базу
 async function save(data) {
   return await fetch(`${process.env.SUPABASE_URL}/rest/v1/records`, {
@@ -45,13 +82,24 @@ app.post("/book", async (req, res) => {
     await save(data);
 
     await sendTG(`
-Новая запись:
-${data.name}
-${data.contact}
-${data.training}
-${data.format}
-${data.date} ${data.time}
+🗓️ Новая запись:
+👤 ${data.name}
+▫️ ${data.contact}
+▫️ ${data.training}
+▫️ ${data.format}
+📆 ${data.date} ${data.time}
     `);
+    scheduleReminder(data.date, data.time, data.chat_id);
+
+    // сообщение клиенту
+await fetch(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
+    chat_id: data.chat_id,
+    text: "Вы успешно записаны на тренировку ✨\n\nЕсли у Вас есть вопросы, свяжитесь со мной @niginacoach"
+  }),
+});
 
     res.json({ ok: true });
 
